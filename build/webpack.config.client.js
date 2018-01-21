c0e770cf271d37b5bfec57ab4e3e8f8d06905709
snowshoe <shaokun.wen@gmail.com>
2018-01-21 13:53:12 +0100

    first commit, webpack dev server done

---
+++ build/webpack.config.client.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  entry: path.join(__dirname, '../client/app.js'),
+  output: path.join(__dirname, '../dist'),
+  modules: path.join(__dirname, '../node_modules'),
+  template: path.join(__dirname, '../client/template.html')
+};
+
+const isDev = process.env.NODE_ENV === 'development';
+const config = {
+  entry: {
+    app: PATHS.entry
+  },
+  output: {
+    //[] means variable in webpack
+    filename: '[name].[hash].js',
+    path: PATHS.output,
+    publicPath: '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        use: 'babel-loader',
+        exclude: [PATHS.modules]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: PATHS.template
+    })
+  ]
+};
+
+if (isDev) {
+  config.devServer = {
+    host: '0.0.0.0',
+    port: '8888',
+    contentBase: PATHS.output
+  };
+}
+
+module.exports = config;
---
+++ build/webpack.config.server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: 'node',
+  entry: {
+    app: path.join(__dirname, '../client/server-entry.js')
+  },
+  output: {
+    //[] means variable in webpack
+    filename: 'server-entry.js',
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        use: 'babel-loader',
+        exclude: [path.join(__dirname, '../node_modules')]
+      }
+    ]
+  }
+};
